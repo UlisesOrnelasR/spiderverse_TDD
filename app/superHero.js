c1790c56e4099e4f1a36a6beae254c8eb80703d1
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-04-18 23:24:34 -0600

    agregando clase superHero y su prueba de unidad

---
+++ app/superHero.js
@@ -0,0 +1,11 @@
+class superHero {
+    constructor(name,actor,age,comic){
+        this.name = name,
+        this.actor = actor,
+        this.age = age,
+        this.comic = comic
+    }
+}
+
+
+module.exports = superHero
--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,15 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+const superHero = require('./../app/superHero')
+
+
+describe("Unit test for superHero", () => {
+    test('Case 1: Get a superHero', () => {
+        //Aqui escribimos el código que queremos usar tal cual
+        const spiderMan = new superHero('Spiderman', 'Tobey M', 37, 'Marvel')
+
+        // Validamos que este codigo funcione de la manera esperada
+        expect(spiderMan.name).toBe('Spiderman')
+        expect(spiderMan.actor).toBe('Tobey M')
+        expect(spiderMan.age).toBe(37)
+        expect(spiderMan.comic).toBe('Marvel')
     });
   })
